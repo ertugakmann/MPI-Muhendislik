eb164f6f9d561d8247d452c755bfab33f9f432b8
ertugakmann <134059022+ertugakmann@users.noreply.github.com>
2024-02-14 18:34:00 +0300

    ScrollReveal javascript codes moved  to another file.

---
+++ js/reveal.js
@@ -0,0 +1,9 @@
+ScrollReveal({
+    distance: "25rem",
+    duration: 1000,
+    delay: 300,
+});
+
+ScrollReveal().reveal(".anime-right", { delay: 300, origin: "right" });
+ScrollReveal().reveal(".anime-left", { delay: 300, origin: "left" });
+ScrollReveal().reveal(".title", { delay: 300, distance: "18px" });
