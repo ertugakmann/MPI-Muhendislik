7d7e88702273ad5f50afa07324515835c6c00329
ertug <ertug3470@gmail.com>
2024-04-01 11:18:36 +0300

    added reveal animation to project's card

--- js/reveal.js
+++ js/reveal.js
@@ -1,9 +1,10 @@
 ScrollReveal({
-    distance: "25rem",
-    duration: 1000,
-    delay: 300,
+  distance: "25rem",
+  duration: 1000,
+  delay: 300,
 });
 
 ScrollReveal().reveal(".anime-right", { delay: 300, origin: "right" });
 ScrollReveal().reveal(".anime-left", { delay: 300, origin: "left" });
 ScrollReveal().reveal(".title", { delay: 300, distance: "18px" });
+ScrollReveal().reveal(".projects-box", { delay: 300, distance: "18px" });
